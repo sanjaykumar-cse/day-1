d290e39bde6db2712937421974edc96aa936f9b4
sanjaykumar-cse <111355256+sanjaykumar-cse@users.noreply.github.com>
2022-09-16 17:21:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+let age =10
+if (age % 2 == 0 ){
+    console.log(age % 2 !=0);
+    console.log("your age is even");
+} else {
+    console.log("your age is odd");
+}
+
+
+var res_str =' ';
+for (var i=1; i<=5; i++) {
+    res_str = res_str + (i * i) + " ";
+}
+console.log(res_str);
